389ae7c500204130fa5afba70ee27d61d48992ef
Jenna Rigby <jennar704@gmail.com>
2026-05-21 11:41:24 +0200

    Edited parameters

--- src/Hyperparameters.js
+++ src/Hyperparameters.js
@@ -3,13 +3,13 @@ const Neighbors = require("./Grid/Neighbors");
 const Hyperparams = {
     setDefaults: function() {
         this.lifespanMultiplier = 100;
-        this.predatorLifespanMultiplier = 4;
-        this.predatorReproductionMultiplier = 15;
+        this.predatorLifespanMultiplier = 5;
+        this.predatorReproductionMultiplier = 10;
         this.predatorDecayRate = 0.5;
         this.predatorStartingFood = 100;
-        this.predatorStarvationBase = 1500; // base ticks before starving without meals
+        this.predatorStarvationBase = 1600; // base ticks before starving without meals
         this.predatorStarvationAgeFactor = 0; // extra tolerance per tick of lifetime
-        this.preyReproductionBonus = 0; // extra food for prey when reproducing, scaled by population density
+        this.preyReproductionBonus = 300; // extra food for prey when reproducing, scaled by population density
 
         this.foodProdProb = 0;
         this.killableNeighbors = Neighbors.adjacent;
