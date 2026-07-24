b37c01afb3f53b73566ea834c04512af769152b7
Jenna Rigby <jennar704@gmail.com>
2026-07-25 00:05:05 +0200

    Changed predator starvation parameter for experiment run V4

--- src/Hyperparameters.js
+++ src/Hyperparameters.js
@@ -7,7 +7,7 @@ const Hyperparams = {
         this.predatorReproductionMultiplier = 26;
         this.predatorDecayRate = 0.5;
         this.predatorStartingFood = 0;
-        this.predatorStarvationBase = 1100; // base ticks before starving without meals
+        this.predatorStarvationBase = 1250; // base ticks before starving without meals
         
 
         this.foodProdProb = 0;
